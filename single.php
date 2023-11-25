0e46afac42bde484e833d2d6b892573f0f3be944
jaycee808 <jamie.clark@live.co.uk>
2023-11-25 20:25:01 +0000

    updated posts, styles, and fonts

--- single.php
+++ single.php
@@ -3,9 +3,10 @@
 <?php 
     while(have_posts()) {
         the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-        
+        <div class="post-content">
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+        </div>
 <?php 
     
 }
