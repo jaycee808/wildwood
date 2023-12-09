98b769546f346081f89e57c4432f49cf553fb6c0
jaycee808 <jamie.clark@live.co.uk>
2023-12-09 20:26:36 +0000

    updated styles

--- footer.php
+++ footer.php
@@ -1,24 +1,13 @@
-<footer class="footer">
-        <div class="footer-content">
-            <div class="footer-info">
-                <div class="footer-heading">Contact Information</div>
-                <p>Wildwood Spa</p>
-                <p>Mulberry Forest</p>
-                <p>Oakwood Lane</p>
-                <p>Ayrshire</p>
-                <p>KA8 1PL</p>
-                <p>Email: info@wildwoodretreat.co.uk</p>
-                <p>Phone: (0156) 885-4512</p>
-            </div>
-            <div class="footer-social">
+<!-- footer -->
+<footer class="page-footer">
+    <div class="footer-text">Copyright &copy; Wildwood.</div>
+    <div class="footer-social">
                 <div class="footer-heading">Follow Us</div>
                 <a href="#" class="social-icon"><i class="fab fa-facebook"></i></a>
                 <a href="#" class="social-icon"><i class="fab fa-twitter"></i></a>
                 <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
             </div>
-        </div>
-    </footer>
-
+</footer>
 </body>
 <?php wp_footer(); ?>
 </html>
--- front-page.php
+++ front-page.php
@@ -10,25 +10,39 @@
     </div>
 </section>
 
-    <!-- <div class="hero-grid">
+<section class="hero-grid">
     <div class="hero-grid-heading">Treatments</div>
         <div class="home-page-grid">
             <div class="grid-item">
                 <div class="grid-heading">Massage</div>
-                <p>Lorem ipsum dolor sit amet, ut usu volumus placerat dignissim, in sea magna errem, eos eu impedit docendi vituperata. Eius volumus delectus mei an. Ne vis delicata definiebas, eos dico ubique volutpat ea. Ut ius veniam libris tibique, eu nam maiorum voluptaria interpretaris.</p>
+                <div class="grid-text">
+                    Experience tranquility with one of our massages. Our Wildwood Signature Massage is a soothing embrace of nature and our Deep Forest Hot Stone Massage uses heated stones strategically placed on key energy points. These treatments will release tension, promote circulation, and envelop you in a state of deep relaxation.
+                </div>
                 <button class="grid-button">
-                    <a href="<?php echo site_url('/treatments'); ?>">Book Now</a>
+                    <a href="<?php echo site_url('/appointments'); ?>">Book Now</a>
                 </button>
             </div>
+
             <div class="grid-item">
-            <div class="grid-heading">Facials</div>
-                <p>Lorem ipsum dolor sit amet. Qui veritatis illo non quia sint aut adipisci aspernatur in laudantium ipsam aut molestiae atque et consequatur modi ut nostrum fugit. Eos veritatis atque est repellat nisi non error explicabo qui sunt odio qui laborum quia aut nisi maiores qui itaque assumenda.</p>
+            <div class="grid-heading">Skincare</div>
+                <div class="grid-text">
+                    Go on a magical journey with our skincare treatments. Our Unicorn Elixir Skincare Ritual is a lavish experience crafted to captivate your senses and our Highland Renewal Scrub is a body treatment inspired by the rugged beauty of the Scottish countryside. Infused with a blend of local herbs and exfoliating elements, these treatments will revitalise your skin.
+                </div>
                 <button class="grid-button">
-                    <a href="<?php echo site_url('/treatments'); ?>">Book Now</a>
+                    <a href="<?php echo site_url('/appointments'); ?>">Book Now</a>
+                </button>
+            </div>
+
+            <div class="grid-item">
+            <div class="grid-heading">Holistic Therapies</div>
+                <div class="grid-text">
+                    Embark on a serene journey with our holistic therapies. Our Forest Bathing Meditation and Mindful Breathing Workshop are guided sessions inspired by the peaceful energy of the forest. These sessions combine ancient techniques with modern mindfulness practices, aiming to promote relaxation, reduce stress, and elevate your overall well-being.
+                </div>
+                <button class="grid-button">
+                    <a href="<?php echo site_url('/appointments'); ?>">Book Now</a>
                 </button>
             </div>
         </div>
-    </div> -->
 </section>
 
 <?php get_footer(); ?>
